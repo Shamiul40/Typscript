0de25c3f2637c914e8e71311ad0428dbce2d2595
Shamiul40 <shamiul4040@gmail.com>
2026-04-14 14:17:55 +0600

    typescript learn

--- Typscript-intro/readme.md
+++ Typscript-intro/readme.md
@@ -8,3 +8,11 @@ unknown dile checking korte hobe
 
 <!-- void -->
 jokon kono function return kore na tokon type void dite hoy.
+
+<!-- undefine -->
+variable declare kora ase, but kono value assign kora nai
+let name ;
+
+<!-- null -->
+null means no value. kono value assign kora nai
+ let age  = null;
--- dataType/src/app.ts
+++ dataType/src/app.ts
@@ -1,13 +1,2 @@
+// Student Profile Project
 
-let studentName : string = "alice"
-
-console.log(studentName) 
-
-const student :  [string, number, boolean] =["alice", 21, true];
-
-const students : Array<string> =["alice", "kob","dob"]
-
-
-function logMessage() :void {
-    console.log("log successfully")
-}
