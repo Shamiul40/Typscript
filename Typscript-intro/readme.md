a0dc675c11b5123a975d32ef12895aca004376a8
Shamiul40 <shamiul4040@gmail.com>
2026-04-14 09:37:26 +0600

    void type

--- Typscript-intro/readme.md
+++ Typscript-intro/readme.md
@@ -2,3 +2,9 @@
 package.json - npm init -y
 tsconfig.json - tsc --init
 dist folder - tsc
+
+<!-- unknown -->
+unknown dile checking korte hobe
+
+<!-- void -->
+jokon kono function return kore na tokon type void dite hoy.
--- dataType/src/app.ts
+++ dataType/src/app.ts
@@ -1,4 +1,13 @@
 
 let studentName : string = "alice"
 
-console.log(studentName)
+console.log(studentName) 
+
+const student :  [string, number, boolean] =["alice", 21, true];
+
+const students : Array<string> =["alice", "kob","dob"]
+
+
+function logMessage() :void {
+    console.log("log successfully")
+}
