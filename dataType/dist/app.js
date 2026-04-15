fc1a154d61bbc044684781d65a6ad5954bb56240
Shamiul40 <shamiul4040@gmail.com>
2026-04-15 23:17:54 +0600

    optional function learning

--- dataType/dist/app.js
+++ dataType/dist/app.js
@@ -1,26 +1,36 @@
 "use strict";
-// Student Profile Project
+// // Student Profile Project
 Object.defineProperty(exports, "__esModule", { value: true });
-const student1 = {
-    name: "Shamiul Islam",
-    age: 27,
-    isEnrolled: true,
-    course: ["javascript", "typescript", "redux"],
-    marks: [88, 90, 92],
-    info: ["shamiul islam", 27, true]
-};
-const student2 = {
-    name: "Rakib",
-    age: 22,
-    isEnrolled: true,
-    course: ["typescript", "javascript", "python"],
-    marks: [70, 75, 80],
-    info: ["Rakib", 22, true]
-};
-function displayStudent(student) {
-    console.log(`name : ${student.name}`);
-    console.log(`age : ${student.age}`);
-    console.log(`course : ${student.course.join(", ")}`);
+// type Student = {
+//     name : string,
+//     age : number,
+//     isEnrolled : boolean,
+//     course : string[],
+//     marks : number[],
+//     info : [string, number, boolean]
+// }
+// const student1 :Student = {
+//     name : "Shamiul Islam",
+//     age :27,
+//     isEnrolled : true,
+//     course : ["javascript", "typescript", "redux"],
+//     marks : [88,90,92],
+//     info : ["shamiul islam", 27, true]
+// }
+// const student2:Student = {
+//     name : "Sakib hassan",
+//     age : 22,
+//     isEnrolled :true,
+//     course: ["typescript", "javascript", "python"],
+//     marks :[70,78,80],
+//     info:["Rakib", 22, false]
+// }
+// function displayStudent(student :Student) :void {
+//     console.log(`name : ${student.name}`)
+// }
+// displayStudent(student1)
+function greetings(firstName, lastName, title) {
+    return `${firstName} ${lastName} he is a ${title ? title : "N/A"}`;
 }
-displayStudent(student1);
+console.log(greetings("Shamiul", "islam", "doctor"));
 //# sourceMappingURL=app.js.map
--- dataType/src/app.ts
+++ dataType/src/app.ts
@@ -1,40 +1,41 @@
-// Student Profile Project
+// // Student Profile Project
 
+// type Student = {
+//     name : string,
+//     age : number,
+//     isEnrolled : boolean,
+//     course : string[],
+//     marks : number[],
+//     info : [string, number, boolean]
+// }
 
+// const student1 :Student = {
+//     name : "Shamiul Islam",
+//     age :27,
+//     isEnrolled : true,
+//     course : ["javascript", "typescript", "redux"],
+//     marks : [88,90,92],
+//     info : ["shamiul islam", 27, true]
+// }
 
-type Student = {
-    name : string,
-    age : number,
-    isEnrolled : boolean,
-    course : string[],
-    marks : number[],
-    info : [string, number, boolean]
+// const student2:Student = {
+//     name : "Sakib hassan",
+//     age : 22,
+//     isEnrolled :true,
+//     course: ["typescript", "javascript", "python"],
+//     marks :[70,78,80],
+//     info:["Rakib", 22, false]
+// }
+
+// function displayStudent(student :Student) :void {
+//     console.log(`name : ${student.name}`)
+
+// }
+
+// displayStudent(student1)
+
+function greetings(firstName: string, lastName: string, title?: string) :string{
+  return `${firstName} ${lastName} he is a ${title ? title : "N/A"}`;
 }
 
-const student1 :Student = {
-    name : "Shamiul Islam",
-    age :27,
-    isEnrolled : true,
-    course : ["javascript", "typescript", "redux"],
-    marks : [88,90,92],
-    info : ["shamiul islam", 27, true]
-}
-
-
-const student2:Student = {
-    name : "Sakib hassan",
-    age : 22,
-    isEnrolled :true,
-    course: ["typescript", "javascript", "python"],
-    marks :[70,78,80],
-    info:["Rakib", 22, false]
-}
-
-
-function displayStudent(student :Student) :void {
-    console.log(`name : ${student.name}`)
-   
-}
-
-
-displayStudent(student1)
+console.log(greetings("Shamiul", "islam", "doctor"))
