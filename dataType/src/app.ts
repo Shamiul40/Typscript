7ca577da01eadf1cd4a9d4bd82026b4dbb49f540
Shamiul40 <shamiul4040@gmail.com>
2026-04-16 21:47:08 +0600

    add function

--- dataType/src/app.ts
+++ dataType/src/app.ts
@@ -1,51 +1,51 @@
-// // Student Profile Project
+// // // Student Profile Project
 
-// type Student = {
-//     name : string,
-//     age : number,
-//     isEnrolled : boolean,
-//     course : string[],
-//     marks : number[],
-//     info : [string, number, boolean]
+// // type Student = {
+// //     name : string,
+// //     age : number,
+// //     isEnrolled : boolean,
+// //     course : string[],
+// //     marks : number[],
+// //     info : [string, number, boolean]
+// // }
+
+// // const student1 :Student = {
+// //     name : "Shamiul Islam",
+// //     age :27,
+// //     isEnrolled : true,
+// //     course : ["javascript", "typescript", "redux"],
+// //     marks : [88,90,92],
+// //     info : ["shamiul islam", 27, true]
+// // }
+
+// // const student2:Student = {
+// //     name : "Sakib hassan",
+// //     age : 22,
+// //     isEnrolled :true,
+// //     course: ["typescript", "javascript", "python"],
+// //     marks :[70,78,80],
+// //     info:["Rakib", 22, false]
+// // }
+
+// // function displayStudent(student :Student) :void {
+// //     console.log(`name : ${student.name}`)
+
+// // }
+
+// // displayStudent(student1)
+
+// // function greetings(firstName: string, lastName: string, title?: string) :string{
+// //   return `${firstName} ${lastName} he is a ${title ? title : "N/A"}`;
+// // }
+
+// // console.log(greetings("Shamiul", "islam", "doctor"))
+
+
+// function weleCome(name:string ="Guest") :string{
+//     return `welcome ${name}`
 // }
-
-// const student1 :Student = {
-//     name : "Shamiul Islam",
-//     age :27,
-//     isEnrolled : true,
-//     course : ["javascript", "typescript", "redux"],
-//     marks : [88,90,92],
-//     info : ["shamiul islam", 27, true]
-// }
-
-// const student2:Student = {
-//     name : "Sakib hassan",
-//     age : 22,
-//     isEnrolled :true,
-//     course: ["typescript", "javascript", "python"],
-//     marks :[70,78,80],
-//     info:["Rakib", 22, false]
-// }
-
-// function displayStudent(student :Student) :void {
-//     console.log(`name : ${student.name}`)
-
-// }
-
-// displayStudent(student1)
-
-// function greetings(firstName: string, lastName: string, title?: string) :string{
-//   return `${firstName} ${lastName} he is a ${title ? title : "N/A"}`;
-// }
-
-// console.log(greetings("Shamiul", "islam", "doctor"))
+// console.log(weleCome())
 
 
-function weleCome(name:string ="Guest") :string{
-    return `welcome ${name}`
-}
-console.log(weleCome())
-
-
-const add=(a:number, b:number) :number=> a+b
-console.log(add(5, 3))
+// const add=(a:number, b:number) :number=> a+b
+// console.log(add(5, 4))
