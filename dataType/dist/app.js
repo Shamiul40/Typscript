8ce20c7f058f16785443092271102aef293575f4
Shamiul40 <shamiul4040@gmail.com>
2026-04-29 21:23:30 +0600

    delte

--- dataType/dist/app.js
+++ dataType/dist/app.js
@@ -109,11 +109,4 @@ user2.name = "Umme Habiba";
 user2.age = 24;
 user2.greet();
 
-const user2 = new person();
-user2.name = "Umme Habiba";
-user2.age = 24;
-user2.greet();
-const user2 = new person();
-user2.name = "Umme Habiba";
-user2.age = 24;
-user2.greet();
+
